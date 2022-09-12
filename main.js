270ce9bb545257e049063402db6ef184697236b4
BMdirac <busarimoruf20@gmail.com>
2022-09-12 22:26:37 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+// *********************** Sets (revision) *********************
+// let inputs = [1,2,4,4,5,1,3]
+
+// function uniqueValue(inputs) {
+//     let array = new Set()
+//     for (let value of inputs) {
+//         array.add(value)
+//         console.log(array)
+//     }
+//     return array
+// }
+
+// uniqueValue(inputs)
+
+// *********************** Map (revision) *********************
+// let input = [1,2,4,4,5,1,3] 
+
+// function myMap(e) {
+//     let map = new Map() 
+//     for (let key of e) {
+//         if(map.has(key)) {
+//             let oldValue =  map.get(key) 
+//             map.set(key, oldValue + 1)
+//         } else {
+//             map.set(key, 1)
+//         }
+//         console.log(map)
+//     }
+//     return map
+// }
+
+// myMap(input)
